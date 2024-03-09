7886e350f14d909a4df2690252c1ee3bf8005250
Fabricio Colombo <fabriciocolombo0096@gmail.com>
2024-03-09 13:28:45 -0300

    x

--- NASA/nasa.py
+++ NASA/nasa.py
@@ -6,22 +6,16 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 response = requests.get(link, headers=headers)
 
 print(response.status_code)
+
 if response.status_code == 200:
     site = bs(response.text, 'html.parser')
     
     titulos = site.find_all('div', class_='hds-a11y-heading-22')
-    num_noticia = 1
-    for titulo in titulos:
-        print(f'Título da notícia {num_noticia}: {titulo.text.strip()}')
-        num_noticia += 1
-    
-    print()
-    
-    conteudo_noticia = 1
     paragrafos = site.find_all('p', class_='margin-top-0 margin-bottom-1')
-    for paragrafo in paragrafos:
-        print(f'Conteúdo da notícia {conteudo_noticia}: {paragrafo.text.strip()}')
-        conteudo_noticia += 1
+    
+    for titulo, paragrafo in zip(titulos, paragrafos):
+        print(f'Título da notícia: {titulo.text.strip()}')
+        print(f'Conteúdo da notícia: {paragrafo.text.strip()}')
         print()
 else:
     print("Erro ao acessar a página:", response.status_code)
